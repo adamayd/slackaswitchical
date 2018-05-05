9f15459317d5fb1c1a38ba0dd3bf5cc3cc226de9
Adam Ayd <adamayd@gmail.com>
2018-05-04 22:00:53 -0400

    fixed conditionals to exit properly

--- filesindir.sh
+++ filesindir.sh
@@ -8,12 +8,12 @@ for i in "${!files[@]}"; do
   printf "%s\t%s\n" "$((i + 1))" "${files[$i]##*/}"
 done
 
-read -p "Enter a number to load the workspace or any other key to exit: " answer
+read -p "Enter a number to load the workspace or Enter to exit: " answer
 
-#if [$answer -lt 1] && [$answer -gt ${#files[@]}]; then
-#  printf "%s\n" "Exiting"
-#  exit 0
-#fi
+if [ ${answer:-0} -lt 1 ] && [ ${answer:-100} -gt ${#files[@]} ]; then
+  printf "%s\n" "Exiting"
+  exit 0
+fi
 
 swapfile() {
   echo "${files[$1]}"
